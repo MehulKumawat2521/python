ea65186fa64ce82df8f5f83176723a33d6b2c35f
Mehul Kumawat <mehulkumawat2521@gmail.com>
2024-12-15 22:34:04 +0530

    Add files via upload

---
+++ 4.py
@@ -0,0 +1,196 @@
+
+# 1. What will be the value of x after the following code?
+# x = 1
+# while x < 3:
+#     x += 1
+# print(x)
+x = 1
+while x < 3:
+    x += 1
+print(x)
+
+# 2. What is the output of the following code?
+# for i in range(4):
+#     print(i)
+# else:
+#     print("End")
+for i in range(4):
+    print(i)
+else:
+    print("End")
+
+# 3. What is the output of the following code?
+# x = 1
+# while x <= 2:
+#     print(x)
+#     x += 1
+#     if x == 2:
+#         continue
+# print("Done")
+x = 1
+while x <= 2:
+    print(x)
+    x += 1
+    if x == 2:
+        continue
+print("Done")
+
+# 4. What is the output of the following code?
+# for i in range(2, 8, 3):
+#     print(i)
+for i in range(2, 8, 3):
+    print(i)
+
+# 5. What is the output of the following code?
+# x = 5
+# for i in range(4):
+#     x += i
+# print(x)
+x = 5
+for i in range(4):
+    x += i
+print(x)
+
+# 6. What is the output of the following code?
+# x = 0
+# for i in range(1, 4):
+#     x += i
+# print(x)
+x = 0
+for i in range(1, 4):
+    x += i
+print(x)
+
+# 7. What is the output of the following code?
+# for i in range(3):
+#     if i == 1:
+#         break
+#     print(i)
+for i in range(3):
+    if i == 1:
+        break
+    print(i)
+
+# 8. What is the output of the following code?
+# x = 4
+# while x > 0:
+#     print(x)
+#     x -= 1
+x = 4
+while x > 0:
+    print(x)
+    x -= 1
+
+# 9. What is the output of the following code?
+# x = 1
+# while x < 3:
+#     x += 1
+#     print(x)
+x = 1
+while x < 3:
+    x += 1
+    print(x)
+
+# 10. What is the output of the following code?
+# x = 10
+# while x >= 5:
+#     print(x)
+#     x -= 2
+x = 10
+while x >= 5:
+    print(x)
+    x -= 2
+
+# 11. What is the output of the following code?
+# for i in range(5, 0, -1):
+#     print(i)
+for i in range(5, 0, -1):
+    print(i)
+
+# 12. What is the output of the following code?
+# x = 0
+# while x < 5:
+#     print(x)
+#     x += 1
+x = 0
+while x < 5:
+    print(x)
+    x += 1
+
+# 13. What is the output of the following code?
+# x = 5
+# for i in range(x):
+#     if i == 3:
+#         continue
+#     print(i)
+x = 5
+for i in range(x):
+    if i == 3:
+        continue
+    print(i)
+
+# 14. What is the output of the following code?
+# x = 7
+# for i in range(4):
+#     x += i
+# print(x)
+x = 7
+for i in range(4):
+    x += i
+print(x)
+
+# 15. What is the output of the following code?
+# x = 2
+# for i in range(3):
+#     x *= i
+# print(x)
+x = 2
+for i in range(3):
+    x *= i
+print(x)
+
+# 16. What is the output of the following code?
+# x = 0
+# while x < 2:
+#     print(x)
+#     x += 1
+x = 0
+while x < 2:
+    print(x)
+    x += 1
+
+# 17. What is the output of the following code?
+# x = 6
+# while x > 0:
+#     print(x)
+#     x -= 2
+x = 6
+while x > 0:
+    print(x)
+    x -= 2
+
+# 18. What is the output of the following code?
+# for i in range(5):
+#     print(i)
+#     if i == 2:
+#         break
+for i in range(5):
+    print(i)
+    if i == 2:
+        break
+
+# 19. What is the output of the following code?
+# x = 3
+# while x < 5:
+#     print(x)
+#     x += 1
+x = 3
+while x < 5:
+    print(x)
+    x += 1
+
+# 20. What is the output of the following code?
+# for i in range(2, 10, 3):
+#     print(i)
+for i in range(2, 10, 3):
+    print(i)
